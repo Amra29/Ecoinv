91e205dd8e5f0d3670072442195dc9d54fc692d5
Amra29 <aaronrugelarevalo@gmailcom>
2026-09-10 21:18:05 -0500

    render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000/api";
-
+const API_BASE_URL = "https://ecoinv-3.onrender.com/api";
 const api = {
     // Helper central para peticiones HTTP
     async request(endpoint, options = {}) {
